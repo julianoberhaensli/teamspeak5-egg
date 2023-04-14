5d7c5428af2ad68572dc832a84338706b52344f6
Julian <julian.oberhaensli@gmail.com>
2023-04-14 05:37:27 +0200

    update to tsserver beta installation (TeamSpeak5)

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # get latest version from github
-LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/KingJP/teamspeak-egg/master/tsversion)
-echo "Latest TeamSpeak3 Version: $LATEST_VERSION"
+LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/julianoberhaensli/teamspeak5-egg/master/tsversion)
+LATEST_TAG=$(curl -s https://raw.githubusercontent.com/julianoberhaensli/teamspeak5-egg/master/tstag)
+echo "Latest TeamSpeak Version: $LATEST_TAG"
 
 # get installed version from version_installed.txt
-INSTALLED_VERSION=$(cat version_installed.txt)
-echo "Installed TeamSpeak3 Version: $INSTALLED_VERSION"
+INSTALLED_TAG=$(cat version_installed.txt)
+echo "Installed TeamSpeak Version: $INSTALLED_TAG"
 
 # check if there is a static version set in pterodactyl panel
 . "version_static.txt"
@@ -18,7 +19,8 @@ then
 fi
 
 updateToVersion() {
-    TSVERSION=$1
+    TSTAG=$1
+    TSVERSION=$2
     echo "cleaning up files before the update..."
     rm -r doc
     rm -r redist
@@ -28,33 +30,30 @@ updateToVersion() {
     rm -r CHANGELOG
     rm ./*.so
     rm ./LICENSE*
-    rm ts3server
+    rm tsserver
     echo "downloading teamspeak version $TSVERSION and extracting file..."
-    curl https://files.teamspeak-services.com/releases/server/"$TSVERSION"/teamspeak3-server_linux_alpine-"$TSVERSION".tar.bz2 | tar xj --strip-components=1
+    curl https://github.com/TeamSpeak-Systems/ts-native/releases/download/"$TSTAG"/teamspeak-server_linux_amd64-"$TSVERSION".tar.bz2 | tar xj --strip-components=1
     echo 'download and extraction finished'
-    chmod +x ts3server_minimal_runscript.sh
+    chmod +x tsserver_minimal_runscript.sh
     echo 'permissions set.'
-    echo '' > .ts3server_license_accepted
+    echo '' > .tsserver_license_accepted
     echo 'accepted license'
     echo "$TSVERSION" > version_installed.txt
     echo 'version written into version_installed.txt file'
 }
 
-if [ "$LATEST_VERSION" != "$INSTALLED_VERSION" ] && [ "$STATIC_VERSION" = 0 ];
+if [ "$LATEST_TAG" != "$INSTALLED_TAG" ] && [ "$STATIC_VERSION" = 0 ];
 then
-    updateToVersion "$LATEST_VERSION"
-elif [ "$SERVER_VERSION" != "$INSTALLED_VERSION" ] && [ "$STATIC_VERSION" = 1 ];
-then
-    updateToVersion "$SERVER_VERSION"
+    updateToVersion "$LATEST_VERSION" "$LATEST_TAG"
 else
     echo 'No update required.'
 fi
 
-if [ ! -f ts3server.ini ]; then
-    ./ts3server_startscript.sh start createinifile=1
+if [ ! -f tsserver.ini ]; then
+    ./tsserver_startscript.sh start createinifile=1
     PID=$(pgrep ts3server)
     kill "$PID"
 fi
 
 echo 'starting server...'
-./ts3server_minimal_runscript.sh inifile=ts3server.ini
+./tsserver_minimal_runscript.sh inifile=tsserver.ini
